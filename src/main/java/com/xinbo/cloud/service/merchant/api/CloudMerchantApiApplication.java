d80c763635dc5de5c1848d8862dc35276a875a3e
smartytony <smartytony2019@gmail.com>
2020-04-07 17:19:23 +0800

    项目调整

--- src/main/java/com/xinbo/cloud/service/merchant/api/CloudMerchantApiApplication.java
+++ src/main/java/com/xinbo/cloud/service/merchant/api/CloudMerchantApiApplication.java
@@ -1,17 +1,15 @@
 package com.xinbo.cloud.service.merchant.api;
 
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(scanBasePackages = "com.xinbo.cloud", exclude= {DataSourceAutoConfiguration.class})
 @EnableSwagger2
-@EnableDubbo
 @MapperScan(basePackages = "com.xinbo.cloud.common.mapper")
+@SpringBootApplication(scanBasePackages = "com.xinbo.cloud", exclude= {DataSourceAutoConfiguration.class})
 public class CloudMerchantApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudMerchantApiApplication.class, args);
